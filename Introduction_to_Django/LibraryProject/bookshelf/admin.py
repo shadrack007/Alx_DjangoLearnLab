a112bec65d715d9339c171ab3cf61956ea3d297a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-12 18:22:47 +0300

    customize admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,7 +3,7 @@ from .models import Book
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_display = ('title', 'author', 'published_year')
     search_fields = ('title', 'author__name')
     list_filter = ('published_date','author')
 
