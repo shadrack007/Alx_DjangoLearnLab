3de3aba64a22ab51963b983e9dc58cd46c318a16
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-19 18:45:23 +0300

    feat: implement auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,7 @@ from django.contrib.auth import login
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_details'),
-    path('auth/login/',login, name='login'),
-    path('auth/login/',LoginView.as_view(template_name="registration/login.html"), name='login'), #redundancy to pass the checker
-    path('auth/logout', LogoutView.as_view(template_name="registration/logout.html"), name='logout'),
-    path('auth/register', views.register, name='register')
+    path('auth/login/',LoginView.as_view(template_name="registration/login.html"), name='login'),
+    path('auth/logout/', LogoutView.as_view(template_name="registration/logout.html"), name='logout'),
+    path('auth/register/', views.register, name='register')
 ]
