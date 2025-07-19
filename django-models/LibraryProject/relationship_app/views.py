69245dff31f7be30e8c1c2c01a64f04166b44027
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-19 16:31:01 +0300

    changed path for the templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,10 +5,9 @@ from .models import Book, Author, Library
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'templates/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailsView(DetailView):
     model = Library
-    template_name = 'templates/library_details.html'
+    template_name = 'relationship_app/library_details.html'
     context_object_name = 'library'
-
