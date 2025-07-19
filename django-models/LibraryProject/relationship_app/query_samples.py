dc5235a26f9b2ca27f0697c62bf16a394e56d092
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-19 16:12:47 +0300

    feat: query data

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,11 @@
-from .models import Book, Librarian, Library
+from .models import Book, Librarian, Library, Author
 
 # List all books in the library
 books = Library.objects.get(name=library_name).books.all()
 
 # Query all books, by a specific author
-author_books = Book.objects.filter(author__name='John Doe').all()
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
 
 # Retrieve the librarian of a specific library
 librarian = Librarian.objects.get(library__name='Central Library')
