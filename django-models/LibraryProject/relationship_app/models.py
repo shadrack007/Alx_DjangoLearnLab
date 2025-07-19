d7e14fb8b22586389ffafabac35ac3fe479e5d55
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-19 15:53:14 +0300

    feat: defined models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,4 +14,4 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
