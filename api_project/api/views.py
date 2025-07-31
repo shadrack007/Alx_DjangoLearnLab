47ce1a436773d301535b4af892c6191ff6ff20e4
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-31 17:07:03 +0300

    _

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookListView(rest_framework.generics.ListAPIView):
+class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
