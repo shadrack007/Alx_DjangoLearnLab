5ece1d15452971ac4f5b82d59273851984fba374
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 16:13:18 +0300

    feat: protect views with csrf tokens

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -3,7 +3,7 @@ from .views import add_book, book_list, edit_book, delete_book
 
 urlpatterns = [
     path('add/', add_book, name='add_book'),
-    path('view/', book_list, name='view_books'),
+    path('view/', book_list, name='book_list'),
     path('edit/<int:book_id>/', edit_book, name='edit_book'),
     path('delete/<int:book_id>/', delete_book, name='delete_book'),
 ]   
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 
 from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 @permission_required('bookshelf.can_add_books', raise_exception=True)
 def add_book(request):
-    return HttpResponse("Book added successfully.")
+    form = BookForm()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+        else:
+            return render(request, 'bookshelf/form_example.html', {
+                'form': form,
+            })
+    else:
+        return render(request, 'bookshelf/form_example.html', {'form': form})
+  
 
 @permission_required('bookshelf.can_view_books', raise_exception=True)
 def book_list(request):
-    return HttpResponse("Here are the books.")
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 
 @permission_required('bookshelf.can_edit_books', raise_exception=True)
 def edit_book(request, book_id):
