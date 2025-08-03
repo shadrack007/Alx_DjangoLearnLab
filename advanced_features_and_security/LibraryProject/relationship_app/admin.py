db99d1cc0e7be71561dc5c02b8b9af98b2ca91e4
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 12:39:45 +0300

    feat: included added filed in model admin

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from  .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'profile_photo', 'date_of_birth')
+
+    
+    
+admin.site.register(CustomUser, CustomUserAdmin)
