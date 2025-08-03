ad54d0d3909c86d7e76bf7c349819e6e9cfa6832
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 14:13:57 +0300

    renamed to booklist

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import add_book, view_books, edit_book, delete_book
+from .views import add_book, book_list, edit_book, delete_book
 
 urlpatterns = [
     path('add/', add_book, name='add_book'),
-    path('view/', view_books, name='view_books'),
+    path('view/', book_list, name='view_books'),
     path('edit/<int:book_id>/', edit_book, name='edit_book'),
     path('delete/<int:book_id>/', delete_book, name='delete_book'),
 ]   
