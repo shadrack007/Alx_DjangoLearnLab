7d576d6b539aff5897fa2012bd6ca51e7ea3ab58
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 13:47:18 +0300

    feat: implemented CRUD on books model

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+
+from .models import Book
 
 # Create your views here.
+@permission_required('bookshelf.can_add_books', raise_exception=True)
+def add_book(request):
+    return HttpResponse("Book added successfully.")
+
+@permission_required('bookshelf.can_view_books', raise_exception=True)
+def view_books(request):
+    return HttpResponse("Here are the books.")
+
+@permission_required('bookshelf.can_edit_books', raise_exception=True)
+def edit_book(request, book_id):
+    return HttpResponse(f"Book {book_id} edited successfully.")
+
+@permission_required('bookshelf.can_delete_books', raise_exception=True)
+def delete_book(request, book_id):
+    return HttpResponse(f"Book {book_id} deleted successfully.")
