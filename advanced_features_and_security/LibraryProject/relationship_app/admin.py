2a8bf198f12f6abb3e5edfe0b5a4fa9a80c4780d
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 12:42:19 +0300

    removed custom user model

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,10 +1,2 @@
 from django.contrib import admin
-from  .models import CustomUser
 
-
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'profile_photo', 'date_of_birth')
-
-    
-    
-admin.site.register(CustomUser, CustomUserAdmin)
