72c3f3e050f884acbdf5d1aa016900d5fa839df7
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-03 16:02:11 +0300

    feat: implemented BookForm

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+        labels = {
+            'title': 'Book Title',
+            'author': 'Author Name',
+            'publication_year': 'Year of Publication',
+        }
