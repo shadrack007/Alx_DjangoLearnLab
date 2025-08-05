5ba34c53488de9536c5895b5ea93331962da2e53
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-05 11:25:01 +0300

    decs: documented the model

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+# Model for Author
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.name   
     
+
+# Model for Book
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
