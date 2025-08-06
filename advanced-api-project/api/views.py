f9b4dec6f75b62101c22d0d6faba62703d9bb69a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-07 00:42:25 +0300

    feat: implemented permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,25 +1,33 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import ReadOnly
 
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = [ReadOnly]
+  
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [ReadOnly]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
