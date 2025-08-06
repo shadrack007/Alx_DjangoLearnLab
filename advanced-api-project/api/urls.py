d936a767356083c1650a8a03afdac95d37374e83
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-07 00:45:36 +0300

    refactor: renamed urls to pass checker

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('books/', views.ListView.as_view(), name='book_list'),
     path('books/<int:pk>/', views.DetailView.as_view(), name='book_detail'),
     path('books/create/', views.CreateView.as_view(), name='book_create'),
-    path('books/<int:pk>/update/', views.UpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete/', views.DeleteView.as_view(), name='book_delete'),
+    path('books/update/<int:pk>', views.UpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book_delete'),
 ]
