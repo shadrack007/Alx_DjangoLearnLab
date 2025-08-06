9e0673d25effe7b314c5013c2713044d087c953a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-07 00:49:05 +0300

    feat: replaced ReadOnly with IsAuthenticatedOrReadOnly

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Book
 from .serializers import BookSerializer
-
-
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
