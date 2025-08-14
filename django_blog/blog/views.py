bcc96ffaab7b1d1222e4e5b716ec53b39adfe42f
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-14 17:42:48 +0300

    feat: implemented login and register

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('home page')
+
+
+def posts(request):
+    return HttpResponse('posts pages')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(data = request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = CustomUserCreationForm(data = request.POST)
+            return render(
+                request, 'blog/register.html', {
+                    "form": form,
+                }
+            )
+    else:
+        form = CustomUserCreationForm()
+
+        return render(
+            request, 'blog/register.html', {
+                "form": form,
+            }
+        )
