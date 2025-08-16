bf733b3842c6c5edbdc5d6d261c3e534144a8f60
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-16 18:34:51 +0300

    feat: CRUD on Post

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('register/', views.register, name = 'register'),
     path('', views.home, name = 'home'),
     #     post
-    path('posts/', views.PostListView.as_view(), name = 'post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name = 'post_create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name = 'post_details'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name = 'post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete'),
+    path('post/', views.PostListView.as_view(), name = 'post_list'),
+    path('post/new/', views.PostCreateView.as_view(), name = 'post_create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name = 'post_details'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name = 'post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete'),
 ]
