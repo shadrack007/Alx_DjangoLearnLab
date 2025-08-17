43ad85535265ff12ce200009265be5cad02319cd
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-17 11:41:22 +0300

    Removed Tag model used only django-taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add = True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    # add tags
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-published_date']
@@ -34,11 +39,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length = 50)
-    post = models.ManyToManyField(Post)
-
-    def __str__(self):
-        return self.name
