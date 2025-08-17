0d7a5a5d749186e87c076c361683624c06242c7f
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-17 12:55:38 +0300

    style: Tag form widget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
+
 from .models import Post, Comment
 
 
@@ -23,6 +25,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            "tags": TagWidget(attrs = {"placeholder": "Add tags..."}),
+        }
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
