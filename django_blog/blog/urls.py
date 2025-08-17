6f7976bd97f6ed55617743e0448f6c9164078094
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-17 11:31:30 +0300

    refactor: renamed urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete'),
 
     #    Comments
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name = 'comment_create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name = 'comment_create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name = 'comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name = 'comment_delete'),
 
